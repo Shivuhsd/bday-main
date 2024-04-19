ceb323bd399ff6c8f3d2da72f7b31c620dcf3aff
Shivakumar <shivu11022002@gmail.com>
2024-04-19 14:47:53 +0530

    new

---
+++ bday-main/assets/js/index.js
@@ -0,0 +1,9 @@
+
+const menu = () => {
+    const btn = document.getElementById('res')
+    if (btn.style.display === "none") {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+}
